cd1f5ec82635f44bef33658ad77d2b171606f64e
Luiz <luiz.souza@gmail.com>
2022-06-21 15:10:36 -0300

    Footer criado c/ três botões. Req 19 ok

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Switch } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
+import Foods from './pages/FoodsPage';
 import UserProvider from './MyContext/UserProvider';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <UserProvider>
       <Switch>
         <Route exact path="/" component={ LoginPage } />
+        <Route exact path="/foods" component={ Foods } />
       </Switch>
     </UserProvider>
   );
