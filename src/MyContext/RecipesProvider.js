f7433ab5f72d2a8690af84c6cc4b18e88302a001
cjheyde <carlaheyde@gmail.com>
2022-06-21 18:24:25 -0300

    chamada de API de categorias

--- src/MyContext/RecipesProvider.js
+++ src/MyContext/RecipesProvider.js
@@ -1,13 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import RecipesContext from './RecipesContext';
 
 function RecipesProvider({ children }) {
-  const [data, setData] = useState([]);
+  const [foodData, setFoodData] = useState([]);
+  const [arrayFoods, setArrayFoods] = useState([]);
+  // const [drinkData, setDrinkData] = useState([]);
+  const [arrayDrinks, setArrayDrinks] = useState([]);
+
+  async function getFoodCategories() {
+    try {
+      const response = await fetch('https://www.themealdb.com/api/json/v1/1/categories.php');
+      const finalData = await response.json();
+      setFoodData(finalData.strCategory);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  useEffect(() => {
+    getFoodCategories();
+  }, []);
 
   const contextType = {
-    data,
-    setData,
+    arrayFoods,
+    setArrayFoods,
+    arrayDrinks,
+    setArrayDrinks,
+    getFoodCategories,
+    foodData,
+    // drinkData,
   };
 
   return (
--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -3,6 +3,19 @@ import React, { useState } from 'react';
 // import { useHistory } from 'react-router-dom';
 
 function Drinks() {
+  const { filteredPlanets } = useContext(PlanetsContext);
+  // console.log(filteredPlanets);
+
+  return (
+    <div>
+      
+          {filteredPlanets !== undefined && filteredPlanets.map((planetData, index) => (
+            <PlanetInformation key={ index } planetData={ planetData } />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 
 }
 
