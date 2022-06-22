467bb7c069ac9b48551aa3c8ae67cc544a0a1d59
Jackson <jviana116@gmail.com>
2022-06-22 14:50:43 -0300

    merge

--- src/pages/ExploreFoods.jsx
+++ src/pages/ExploreFoods.jsx
@@ -2,11 +2,9 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-// import RecipesContext from '../MyContext/RecipesContext';
 
 function ExploreFoods() {
   const history = useHistory();
-  // const { randomFoodAndDrinks } = useContext(RecipesContext);
   const randomClick = async () => {
     try {
       const urlRandom = 'https://www.themealdb.com/api/json/v1/1/random.php';
