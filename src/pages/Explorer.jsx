0b7946a9becc724ed24fb2788d4df527661d933a
Luiz <luiz.souza@gmail.com>
2022-06-21 22:00:25 -0300

    Página Explorer feita. Reqs 67-68-69 ok

--- src/pages/Explorer.jsx
+++ src/pages/Explorer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
@@ -6,6 +7,24 @@ function Explore() {
   return (
     <>
       <Header />
+      <Link to="/explore/foods">
+        <button
+          type="button"
+          data-testid="explore-foods"
+          src="src/images/drinkIcon.svg"
+        >
+          Explore Foods
+        </button>
+      </Link>
+      <Link to="/explore/drinks">
+        <button
+          type="button"
+          data-testid="explore-drinks"
+          src="src/images/drinkIcon.svg"
+        >
+          Explore Drinks
+        </button>
+      </Link>
       <Footer />
     </>
   );
