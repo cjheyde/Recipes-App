0283db3f5bbcc44a60fe9277554d4980ee5259df
Luiz <luiz.souza@gmail.com>
2022-06-21 21:45:02 -0300

    Componente Footer completo, Reqs 19 até 24 ok

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../CSS/Footer.css';
+import { Link } from 'react-router-dom';
 import drinkIcon from '../images/drinkIcon.svg';
 import exploreIcon from '../images/exploreIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
@@ -9,27 +10,33 @@ export default function Footer() {
     <footer
       data-testid="footer"
     >
-      <button
-        type="button"
-        data-testid="drinks-bottom-btn"
-        src="src/images/drinkIcon.svg"
-      >
-        <img src={ drinkIcon } alt="Bebidas" />
-      </button>
-      <button
-        type="button"
-        data-testid="explore-bottom-btn"
-        src="src/images/exploreIcon.svg"
-      >
-        <img src={ exploreIcon } alt="Explorar" />
-      </button>
-      <button
-        type="button"
-        data-testid="food-bottom-btn"
-        src="src/images/mealIcon.svg"
-      >
-        <img src={ mealIcon } alt="Comidas" />
-      </button>
+      <Link to="/drinks">
+        <button
+          type="button"
+          data-testid="drinks-bottom-btn"
+          src="src/images/drinkIcon.svg"
+        >
+          <img src={ drinkIcon } alt="Bebidas" />
+        </button>
+      </Link>
+      <Link to="/explore">
+        <button
+          type="button"
+          data-testid="explore-bottom-btn"
+          src="src/images/exploreIcon.svg"
+        >
+          <img src={ exploreIcon } alt="Explorar" />
+        </button>
+      </Link>
+      <Link to="/foods">
+        <button
+          type="button"
+          data-testid="food-bottom-btn"
+          src="src/images/mealIcon.svg"
+        >
+          <img src={ mealIcon } alt="Comidas" />
+        </button>
+      </Link>
     </footer>
   );
 }
